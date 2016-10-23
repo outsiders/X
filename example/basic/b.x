54098cd1629e84d0c09ec887aa2d51ac4dfe0370
zyp001a <yzhou01a@gmail.com>
2016-10-23 18:16:14 +0800

    fix function default bug

--- example/basic/b.x
+++ example/basic/b.x
@@ -4,11 +4,11 @@ a;
 b = @x:number x;
 b 1;
 
-c = @x:number?1 ~number x+1;
+c = ~number @x:number?1 x+1;
 c x:1;
 b (c);
 
-d = class x:1 @x;
+d = :class @x:number?1;
 e = new d;
 e.x;
 f = new d 2;
--- example/basic/index.js
+++ example/basic/index.js
@@ -1,39 +1,22 @@
-var a, b, c, d, x;
-console.log("a");
-a = "x";
-console.log(a);
+var a, b, c, d, e, f;
+a = 1;
+a;
 b = function(x){
-	var y, z;
-	y = 1;
-	z = 2;
-	return x+y+z;
+	return x;
 };
 b(1);
-b(1);
-c = function(){
+c = function(x){
+	if(x === undefined) x = 1;
+	return x+1;
+};
+c(1);
+b(c());
+d = function(){
 	var self = this;
 	self.x = arguments[0];
 	if(self.x === undefined) self.x = 1;
 };
-d = new c(1);
-d["x"];
-d["x"];
-d[a];
-for(x = 1;x < 3;x = x+1){
-	console.log(x);
-	x = x+1;
-};
-var _ta0 = [1, 2, 3];
-var _ti0;
-for(_ti0 = 0; _ti0 < _ta0.length; _ti0++){
-	x = _ta0[_ti0];
-	console.log(x);
-};
-if(x){
-	console.log(x);
-};
-if(a){
-	console.log(a);
-}else{
-	console.log(b);
-};
+e = new d();
+e["x"];
+f = new d(2);
+f["x"];
